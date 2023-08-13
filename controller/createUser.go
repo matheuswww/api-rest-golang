37785745a1b87f4bc990661c7cf62ef5c47ac77a
virussv <teteus.30.08.07@outlook.com>
2023-08-12 21:43:13 -0300

    created personalized logger

--- controller/createUser.go
+++ controller/createUser.go
@@ -1,16 +1,35 @@
 package controller
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/virussv/api-rest-golang/configuration/logger"
 	"github.com/virussv/api-rest-golang/configuration/validation"
 	"github.com/virussv/api-rest-golang/model/request"
+	"github.com/virussv/api-rest-golang/model/response"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+	zap.String("journey","createuser"),
+)
 	var userRequest request.UserRequest;
 	if err := c.ShouldBindJSON(&userRequest);err != nil {
+		logger.Error("Error trying to validate user info",err)
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code,restErr)
 		return
 	}
+	response := response.UserResponse  {
+		Email: userRequest.Email,
+		ID: "Test",
+		Name: userRequest.Name,
+		Age: userRequest.Age,
+	}
+	logger.Info("User created succesfully",
+	zap.String("journey","createuser"),
+)
+	c.JSON(http.StatusOK,response)
 }
--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	"log"
+
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/virussv/api-rest-golang/configuration/logger"
 	"github.com/virussv/api-rest-golang/routes"
 )
 
 func main() {
+	logger.Info("About to start user applicataion")
 	err := godotenv.Load();
 	if err != nil {
 		log.Fatal("Error loading .env file")
