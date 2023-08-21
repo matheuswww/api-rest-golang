ef0b1610b3e32dc3f9e72e5f9720e66e43ae617d
virussv <teteus.30.08.07@outlook.com>
2023-08-21 14:49:58 -0300

    unit tests created for findUserByEmailAndPassword repository

--- src/model/repository/find_user_repository_test.go
+++ src/model/repository/find_user_repository_test.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"errors"
+	"regexp"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -68,4 +69,65 @@ func TestUserRepository_FindUserByEmail(t *testing.T) {
 		assert.NotNil(t,err)
 		assert.Nil(t,userDomain)
 	})
+
+	//find user by email and password
+	t.Run("Success", func(t *testing.T) {
+		db, mock, err := sql_mock.NewMockedMysqlConnection()
+		if err != nil {
+			t.Fatalf("Erro ao criar conexão falsa com o banco de dados: %v", err)
+		}
+		defer db.Close()
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT email,password,name,age,id FROM users WHERE email = ? AND password = ?")).
+			WithArgs("test@test.com","test").
+			WillReturnRows(sqlmock.NewRows([]string{"email", "password", "name", "age", "id"}).
+			AddRow("test@test.com", "test", "test", uint8(90), 1))
+
+		repo := NewUserRepository(db)
+		userDomain, err := repo.FindUserByEmailAndPassword("test@test.com","test")
+
+		assert.Nil(t, err)
+		assert.NotNil(t, userDomain.GetId())
+		assert.EqualValues(t, userDomain.GetEmail(), "test@test.com")
+		assert.EqualValues(t, userDomain.GetName(), "test")
+		assert.EqualValues(t, userDomain.GetPassword(), "test")
+		assert.EqualValues(t, userDomain.GetAge(), uint8(90))
+	})
+
+	t.Run("return_error_from_database", func(t *testing.T) {
+		db, mock, err := sql_mock.NewMockedMysqlConnection()
+		if err != nil {
+			t.Fatalf("Erro ao criar conexão falsa com o banco de dados: %v", err)
+		}
+		defer db.Close()
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT email,password,name,age,id FROM users WHERE email = ? AND password = ?")).
+			WithArgs("test@test.com","test").
+			WillReturnError(errors.New("database error"))
+
+		repo := NewUserRepository(db)
+		userDomain, err := repo.FindUserByEmailAndPassword("test@test.com","test")
+
+		assert.NotNil(t, err)
+		assert.Nil(t, userDomain)
+	})
+
+	t.Run("error_user_notfound", func(t *testing.T) {
+		db, mock, err := sql_mock.NewMockedMysqlConnection()
+		if err != nil {
+			t.Fatalf("Erro ao criar conexão falsa com o banco de dados: %v", err)
+		}
+		defer db.Close()
+
+		mock.ExpectQuery(regexp.QuoteMeta("SELECT email,password,name,age,id FROM users WHERE email = ? AND password = ?")).
+			WithArgs("test@test.com","test").
+			WillReturnRows(sqlmock.NewRows([]string{}))
+	
+		repo := NewUserRepository(db)
+		userDomain, err := repo.FindUserByEmailAndPassword("test@test.com","test")
+
+		assert.NotNil(t,err)
+		assert.Nil(t,userDomain)
+	})
+	
 }
